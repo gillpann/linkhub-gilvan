cef6fc56d56158fb625617ae9d8238aaebc404d8
gillpann <cpundong@gmail.com>
2025-02-24 14:46:46 +0700

    update footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
-        <footer className="w-full text-center py-6 text-[var(--text-muted)] text-sm relative z-10">
-        <p>© {new Date().getFullYear()} Created by Ahmad Gilvan. All rights reserved.</p>
+        <footer className="w-full text-center py-6 text-[var(--text-muted)] text-sm md:text-base whitespace-nowrap relative z-10">
+        <p> &copy; {new Date().getFullYear()} Created by Ahmad Gilvan. All rights reserved.</p>
         </footer>
     );
 };
